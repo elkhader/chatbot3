0f8bb279e825c35e823c5368d4c0b045b4206cb8
srelkha <sr.elkhader@gmail.com>
2020-06-13 14:48:44 +0000

    first commit

---
+++ app/src/main/java/com/example/chatbot3/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.chatbot3
+
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.github.bassaer.chatmessageview.model.ChatUser
+import com.github.bassaer.chatmessageview.model.Message
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.json.responseJson
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val ACCESS_TOKEN = "8278ba5833f844fe898d1dc99499c8d4"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FuelManager.instance.baseHeaders = mapOf(
+            "Authorization" to "Bearer $ACCESS_TOKEN"
+        )
+
+        FuelManager.instance.basePath = "https://api.dialogflow.com/v1/"
+
+        FuelManager.instance.baseParams = listOf(
+            "v" to "20170712",                  // latest protocol
+            "sessionId" to UUID.randomUUID(),   // random Id
+            "lang" to "en"                      // English language
+        )
+
+        val human = ChatUser(
+            1,
+            "You",
+            BitmapFactory.decodeResource(resources,
+                R.drawable.ic_account_circle)
+        )
+
+        val agent = ChatUser(
+            2,
+            "Agent",
+            BitmapFactory.decodeResource(resources,
+                R.drawable.ic_account_circle)
+        )
+
+        my_chat_view.setOnClickSendButtonListener(View.OnClickListener {
+            my_chat_view.send(Message.Builder()
+                .setUser(human)
+                .setText(my_chat_view.inputText)
+                .build()
+            )
+            Fuel.get("/query",
+                listOf("query" to my_chat_view.inputText))
+                .responseJson { _, _, result ->
+                    val reply = result.get().obj()
+                        .getJSONObject("result")
+                        .getJSONObject("fulfillment")
+                        .getString("speech")
+
+                    my_chat_view.send(Message.Builder()
+                        .setRight(true)
+                        .setUser(agent)
+                        .setText(reply)
+                        .build()
+                    )
+
+                    val intent:String? = result.get().obj()
+                        .getJSONObject("result")
+                        .optJSONObject("metadata")
+                        .optString("intentName")
+
+                    if(intent!! == "WEIGHT") {
+                        val unitWeightName = result.get().obj()
+                            .getJSONObject("result")
+                            .getJSONObject("parameters")
+                            .getString("unit-weight-name")
+
+                        val unitWeight = result.get().obj()
+                            .getJSONObject("result")
+                            .getJSONObject("parameters")
+                            .getJSONObject("unit-weight")
+                            .getDouble("amount")
+
+                        val result = if(unitWeightName == "lb") {
+                            unitWeight * 2.20462
+                        } else {
+                            unitWeight / 2.20462
+                        }
+
+                        my_chat_view.send(Message.Builder()
+                            .setRight(true)
+                            .setUser(agent)
+                            .setText("That's ${"%.2f".format(result)} $unitWeightName")
+                            .build()
+                        )
+                    }
+                }
+        })
+    }
+}
